51da3e3c76309f8bbde38dfe3c9d94e428f77807
Iulian Rotaru <iulian@gnotis.ro>
2019-12-03 19:31:18 +0200

    Add image file info pattern for RGB 256 colors.

--- atlas-studio-service/src/main/java/com/kidscademy/atlas/studio/tool/ImageInfoResult.java
+++ atlas-studio-service/src/main/java/com/kidscademy/atlas/studio/tool/ImageInfoResult.java
@@ -22,8 +22,9 @@ public class ImageInfoResult implements ResultParser {
     // 4. picture.png PNG 1723x1853 1723x1853-450-293 8-bit sRGB 946990B 0.000u 0:00.000
     // # next pattern has file name with space
     // 5. picture file.jpg JPEG 920x560 920x560+0+0 8-bit sRGB 178854B 0.047u 0:00.045
+    // 6. picture.png PNG 400x400 400x400+0+0 8-bit sRGB 256c 71081B 0.000u 0:00.000
     private static final Pattern FORMAT = Pattern.compile(
-	    "^(.+) (JPEG|PNG) (\\d+)x(\\d+) (\\d+)x(\\d+)[+-](\\d+)[+-](\\d+) (8|16)-bit (sRGB|Grayscale Gray) (\\d+(?:\\.\\d+)?)(B|KiB|MiB) .+$");
+	    "^(.+) (JPEG|PNG) (\\d+)x(\\d+) (\\d+)x(\\d+)[+-](\\d+)[+-](\\d+) (8|16)-bit (sRGB|sRGB 256c|Grayscale Gray) (\\d+(?:\\.\\d+)?)(B|KiB|MiB) .+$");
 
     @Override
     public void parse(String line) {
