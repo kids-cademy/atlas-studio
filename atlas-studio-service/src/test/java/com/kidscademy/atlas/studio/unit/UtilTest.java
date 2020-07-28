bf577e8a9f6a40893bf11887a9c3f2cb9f83f880
Iulian Rotaru <iulian@gnotis.ro>
2020-07-28 20:18:28 +0300

    Fix windows dependency on unit test for utility classes.

--- atlas-studio-service/src/test/java/com/kidscademy/atlas/studio/unit/UtilTest.java
+++ atlas-studio-service/src/test/java/com/kidscademy/atlas/studio/unit/UtilTest.java
@@ -1,6 +1,7 @@
 package com.kidscademy.atlas.studio.unit;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.endsWith;
+
 import static org.junit.Assert.assertThat;
 
 import java.io.File;
@@ -13,7 +14,7 @@ public class UtilTest {
     @Test
     public void OS_escapePath() {
 	String os = System.setProperty("os.name", "win");
-	assertThat(OS.escapePath(new File("D:\\tmp\\apps\\lib")), equalTo("D:\\\\tmp\\\\apps\\\\lib"));
+	assertThat(OS.escapePath(new File("D:\\tmp\\apps\\lib")), endsWith("D:\\\\tmp\\\\apps\\\\lib"));
 	System.setProperty("os.name", os);
     }
 }
