c92ec481c84978db06f9b914a84368eba476b6cd
Iulian Rotaru <iulian@gnotis.ro>
2020-08-29 12:08:31 +0300

    Add string representation for console message.

--- atlas-studio-service/src/main/java/com/kidscademy/atlas/studio/tool/ConsoleMessage.java
+++ atlas-studio-service/src/main/java/com/kidscademy/atlas/studio/tool/ConsoleMessage.java
@@ -2,11 +2,16 @@ package com.kidscademy.atlas.studio.tool;
 
 import js.lang.Event;
 
-public class ConsoleMessage implements Event {
-    @SuppressWarnings("unused")
-    private final String text;
+public class ConsoleMessage implements Event
+{
+  private final String text;
 
-    public ConsoleMessage(String text) {
-	this.text = text;
-    }
+  public ConsoleMessage(String text) {
+    this.text = text;
+  }
+
+  @Override
+  public String toString() {
+    return text;
+  }
 }
